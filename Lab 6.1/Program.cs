0ee94c718efe973095bdc7050cf24afafe71a632
krist <krist@LAPTOP-HIQLHLL6>
2022-11-09 18:46:07 +0500

    Добавьте файлы проекта.

---
+++ Lab 6.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] strArray = str.Split();
+            string max = strArray[0];
+            foreach (string s in strArray)
+            {
+                if (s.Length > max.Length)
+                {
+                    max = s;
+                }
+            }
+            Console.WriteLine(max);
+            Console.ReadKey();
+
+        }
+    }
+}
